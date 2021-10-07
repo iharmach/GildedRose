a477dedb1ebeecc3bb714c055d9428997fb30513
Ilham Harmach <iharmach@idmog.com>
2021-10-07 18:46:05 +0200

    [FEAT] Add tests before changing the script

--- tests/test.js
+++ tests/test.js
@@ -1,22 +1,104 @@
 const { Shop, Item } = require("../src/gilded_rose");
 
 describe("Gilded Rose", function () {
-  it("should foo", function () {
+  it("should not have quality inferior to 0", function () {
     const items = [
-      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("+5 Dexterity Vest", 10, 0),
       new Item("Aged Brie", 2, 0),
-      new Item("Elixir of the Mongoose", 5, 7),
-      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      new Item("Elixir of the Mongoose", 5, 0),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 0),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 0),
       // this conjured item does not work properly yet
-      new Item("Conjured Mana Cake", 3, 6)
+      new Item("Conjured Mana Cake", 3, 6),
     ];
 
     const gildedRose = new Shop(items);
     const updatedItems = gildedRose.updateQuality();
-    expect(updatedItems[0].name).toBe("fixme");
+    for (const updatedItem of updatedItems) {
+      expect(updatedItem.quality).toBeGreaterThanOrEqual(0);
+    }
+  });
+});
+
+describe("Aged Brie", () => {
+  it("should update if quality is > 50 and sellIn is > 0", () => {
+    const items = [new Item("Aged Brie", 3, 50)];
+
+    const gildedRose = new Shop(items);
+    const updatedItems = gildedRose.updateQuality();
+
+    expect(updatedItems[0].sellIn).toEqual(2);
+    expect(updatedItems[0].quality).toEqual(50);
+  });
+
+  it("should update if quality is < 50 and sellIn is > 0", () => {
+    const items = [new Item("Aged Brie", 2, 30)];
+
+    const gildedRose = new Shop(items);
+    const updatedItems = gildedRose.updateQuality();
+
+    expect(updatedItems[0].sellIn).toEqual(1);
+    expect(updatedItems[0].quality).toEqual(31);
+  });
+});
+
+describe("Backstage passes to a TAFKAL80ETC concert", () => {
+  it("should update if quality is < 50 and 6<sellIn< 11", () => {
+    const items = [
+      new Item("Backstage passes to a TAFKAL80ETC concert", 7, 38),
+    ];
+
+    const gildedRose = new Shop(items);
+    const updatedItems = gildedRose.updateQuality();
+
+    expect(updatedItems[0].sellIn).toEqual(6);
+    expect(updatedItems[0].quality).toEqual(40);
+  });
+
+  it("should update if quality is < 50 and 0<sellIn<6", () => {
+    const items = [
+      new Item("Backstage passes to a TAFKAL80ETC concert", 2, 38),
+    ];
+
+    const gildedRose = new Shop(items);
+    const updatedItems = gildedRose.updateQuality();
+
+    expect(updatedItems[0].sellIn).toEqual(1);
+    expect(updatedItems[0].quality).toEqual(41);
+  });
+
+  it("should update if sellIn is negative", () => {
+    const items = [
+      new Item("Backstage passes to a TAFKAL80ETC concert", -1, 38),
+    ];
+
+    const gildedRose = new Shop(items);
+    const updatedItems = gildedRose.updateQuality();
+
+    expect(updatedItems[0].sellIn).toEqual(-2);
+    expect(updatedItems[0].quality).toEqual(0);
+  });
+});
+describe("Elixir of the Mongoose", () => {
+  it("should update", () => {
+    const items = [new Item("Elixir of the Mongoose", 3, 5)];
+
+    const gildedRose = new Shop(items);
+    const updatedItems = gildedRose.updateQuality();
+
+    expect(updatedItems[0].sellIn).toEqual(2);
+    expect(updatedItems[0].quality).toEqual(4);
+  });
+});
+
+describe("Sulfuras, Hand of Ragnaros", () => {
+  it("should not update", () => {
+    const items = [new Item("Sulfuras, Hand of Ragnaros", 5, 80)];
+
+    const gildedRose = new Shop(items);
+    const updatedItems = gildedRose.updateQuality();
+
+    expect(updatedItems[0].sellIn).toEqual(5);
+    expect(updatedItems[0].quality).toEqual(80);
   });
 });
