158a342c08237abca4836f33272115bd69005657
Ilham Harmach <iharmach@idmog.com>
2021-10-07 19:26:03 +0200

    [REFACTOR] First refactoring

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,58 +10,37 @@ class Shop {
   constructor(items = []) {
     this.items = items;
   }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name !== "Aged Brie" &&
-        this.items[i].name !== "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name !== "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name === "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
+      if (this.items[i].name === "Sulfuras, Hand of Ragnaros") {
+        continue;
       }
-      if (this.items[i].name !== "Sulfuras, Hand of Ragnaros") {
+
+      if (this.items[i].name == "Aged Brie") {
+        this.items[i].quality = Math.min(50, this.items[i].quality + 1);
         this.items[i].sellIn = this.items[i].sellIn - 1;
+        continue;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name !== "Aged Brie") {
-          if (
-            this.items[i].name !== "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name !== "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
+
+      if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+        const sellIn = this.items[i].sellIn;
+        const concertbetween6And10Days = sellIn >= 6 && sellIn < 11 ? 1 : 0;
+        const concertInLessThan6Days = sellIn < 6 && sellIn >= 0 ? 1 : 0;
+        const concertFinished = sellIn < 0 ? 1 : 0;
+        this.items[i].quality +=
+          concertbetween6And10Days * 2 +
+          concertInLessThan6Days * 3 -
+          concertFinished * this.items[i].quality;
+        this.items[i].quality = Math.min(50, this.items[i].quality);
+
+        continue;
+      }
+
+      if (this.items[i].quality > 0) {
+        this.items[i].quality = this.items[i].quality - 1;
+        this.items[i].sellIn = this.items[i].sellIn - 1;
       }
     }
 
@@ -71,5 +50,5 @@ class Shop {
 
 module.exports = {
   Item,
-  Shop
+  Shop,
 };
